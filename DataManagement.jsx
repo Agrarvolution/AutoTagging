829b7b2ae59a823a8cec82284bdf7fafd2471aa0
Mueggrogondrolla <S1610238045@students.fh-hagenberg.at>
2018-11-26 17:51:03 +0100

    rename + adding label pseudo class

---
+++ DataManagement.jsx
@@ -0,0 +1,54 @@
+﻿/**
+
+ File zur Verwaltung aller Daten, die bei der Ver- und Bearbeitung der Tags anfallen
+
+ */
+
+#include 'EventManager.jsinc'
+#include "js/libs/json2.js"  
+
+var jsonAWS;
+var jsonVision;
+
+function main(path)
+{
+    var image = getImage(path);
+
+    jsonAWS = sendToAWS(path);
+    jsonVision = sendToVision(path);
+
+
+}
+
+function getImagePath()
+{
+
+}
+
+function findLabels()
+{
+    
+}
+
+function sendToAWS(image)
+{
+
+}
+
+function sendToVision(image)
+{
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+main(path);
--- Label.js
+++ Label.js
@@ -1,7 +1,39 @@
-/**
- * Created by Matthias on 26.11.2018.
- */
+#include "js/libs/json2.js"  
 
-function Label() {
+function Label(name, confidence, parents)
+{
+    this.name = name;
+    this.confidence = confidence;
+    this.parents = parents;
+}
+
+Label.prototype.toJSON = function()
+{
+    var parentsJSON = { parents : [] };
+
+    for (var i = 0; i < this.parents.length; i++)
+    {
+        const currentParentJSON = this.parents[i].toJSON();
+        
+        parentsJSON.parents.concat(currentParentJSON);
+    }
+
+    return JSON.stringify({name : this.name, confidence : this.confidence, parents : parentsJSON});
+}
+
+Label.prototype.square = function()
+{
+    this.confidence *= this.confidence;
+
+    this.clamp();
+}
+
+Label.prototype.clamp = function()
+{
+    this.confidence = Math.min(Math.max(parseFloat(this.confidence), 0), 1);
+}
+
+Label.prototype.sanitize = function()
+{
     
 }
