739939312f948fa9fd236360f1749809bc537ec1
Mai <carolin.schuntermann@gmail.com>
2018-11-27 15:18:15 +0100

    update - the path needs to have '/', not '\'

--- AWS/script.js
+++ AWS/script.js
@@ -1,5 +1,5 @@
 var getLabels = require('./RekognitionLabels')
 
-url = 'D:\\Carolin\\Documents\\_MTD\\5_Semester\\PRO5\\AWS\\img\\noticed.jpg';
+url = 'D:/Carolin/Art/My Art/_Zoids/__Mai Zoids/Belial 1_1024.jpg';
 
 getLabels(url).then((response) => console.log(response));
