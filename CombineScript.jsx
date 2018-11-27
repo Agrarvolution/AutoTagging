a1b58ff7f6a251105c8685cfdfdba194bb075124
Mueggrogondrolla <S1610238045@students.fh-hagenberg.at>
2018-11-27 11:45:22 +0100

    Data management + added combinescript

---
+++ CombineScript.jsx
@@ -0,0 +1,41 @@
+
+
+/**
+ * Processes responses from Google Vision and Amazon Rekognition
+ * @return array with combined values and confidences
+ * @param {array} visionResponse 
+ * @param {array} rekognitionResponse 
+ */
+function processResponses(visionResponse, rekognitionResponse)
+{
+    var visionObject = handleVisionResponse(visionResponse);
+    var rekognitionObject = handleRekognitionResponse(rekognitionResponse);
+    var outputObject = [];
+
+    if (typeof visionObject !== 'undefined' && visionObject.length > 0 && typeof rekognitionObject !== 'undefined' && rekognitionObject.length > 0)
+    {
+        for (var i = 0; i < visionObject.length; i++)
+        {
+            var matchingIndex = searchInDescription(rekognitionObject, visionObject[i].description);
+            if (matchingIndex >= 0)
+            {
+                var tempObject = rekognitionObject[matchingIndex];
+                rekognitionObject.splice[matchingIndex,1]; //doesn't take Vision parents in account
+                tempObject.confidence*= visionObject[i].confidence;
+                outputObject.push(tempObject);
+                visionObject.splice(i--,1);
+            }
+        }
+        outputObject.concat(squareConfidence(visionObject));
+        outputObject.concat(squareConfidence(rekognitionObject));
+    }
+    else if (typeof visionObject !== 'undefined' && visionObject.length > 0)
+    {
+        outputObject = squareConfidence(rekognitionObject);
+    }
+    else if (typeof rekognitionObject !== 'undefined' && rekognitionObject.length > 0)
+    {
+        outputObject = squareConfidence(visionObject);
+    }
+    return outputObject;
+}
--- ModifyTags.jsx
+++ ModifyTags.jsx
@@ -198,46 +198,6 @@ function handleRekognitionResponse(responseJSON)
 }
 
 /**
- * Processes responses from Google Vision and Amazon Rekognition
- * @return array with combined values and confidences
- * @param {array} visionResponse 
- * @param {array} rekognitionResponse 
- */
-function processResponses(visionResponse, rekognitionResponse)
-{
-    visionObject = handleVisionResponse(visionResponse);
-    rekognitionObject = handleRekognitionResponse(rekognitionResponse);
-    outputObject = [];
-
-    if (typeof visionObject !== 'undefined' && visionObject.length > 0 && typeof rekognitionObject !== 'undefined' && rekognitionObject.length > 0) 
-    {
-        for (var i = 0; i < visionObject.length; i++)
-        {
-            var matchingIndex = searchInDescription(rekognitionObject, visionObject[i].description);
-            if (matchingIndex >= 0)
-            {
-                var tempObject = rekognitionObject[matchingIndex];
-                rekognitionObject.splice[matchingIndex,1]; //doesn't take Vision parents in account
-                tempObject.confidence*= visionObject[i].confidence;
-                outputObject.push(tempObject);
-                visionObject.splice(i--,1);
-            }
-        }
-        outputObject.concat(squareConfidence(visionObject));
-        outputObject.concat(squareConfidence(rekognitionObject));
-    }
-    else if (typeof visionObject !== 'undefined' && visionObject.length > 0)
-    {
-        outputObject = squareConfidence(rekognitionObject);
-    }
-    else if (typeof rekognitionObject !== 'undefined' && rekognitionObject.length > 0)
-    {
-        outputObject = squareConfidence(visionObject);
-    }
-    return outputObject;
-}
-
-/**
  * Squares all confidence levels in the array
  * @param {array} array 
  */
