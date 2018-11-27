6562509cd1de477075616a22ae7a72e1d4682cf4
Mueggrogondrolla <S1610238045@students.fh-hagenberg.at>
2018-11-27 17:04:23 +0100

    datamanagement runfähig machen #3

--- AWS/RekognitionLabels.js
+++ AWS/RekognitionLabels.js
@@ -1,18 +1,18 @@
 function RecognitionLabels()
 {
-    
+
 }
 
 RecognitionLabels.prototype.getLabels = function(path)
 {
-    return new Promise(resolve =>
+    return new Promise(function resolve()
         {
         // Load the SDK and base64encoder
         const AWS = require('aws-sdk');
         const image2base64 = require('image-to-base64');
         
         image2base64(path) // you can also to use url
-            .then((response) =>
+            .then(function(response)
             {
                 // console.log(response); //cGF0aC90by9maWxlLmpwZw==
                 var params =
@@ -26,12 +26,12 @@ RecognitionLabels.prototype.getLabels = function(path)
                 };
                 
                 var rekognition = new AWS.Rekognition({ apiVersion: '2016-06-27', region: 'us-west-2' });
-                rekognition.detectLabels(params, (err, data) =>
+                rekognition.detectLabels(params, function(err, data)
                 {
                     if (err)
                     {
                         console.error(err);
-                        // console.error(err.stack);
+                        // console.error(err.stack);S
                         resolve(-1); // ERROR!
                     }
                     else
@@ -41,8 +41,9 @@ RecognitionLabels.prototype.getLabels = function(path)
                     }
                 });
             })
-            .catch((error) => {
-                console.log(error); //Exception error....
+            .catch(function(error)
+            {
+                //console.log(error); //Exception error....
                 resolve(-1); // ERROR!
             });
     });
