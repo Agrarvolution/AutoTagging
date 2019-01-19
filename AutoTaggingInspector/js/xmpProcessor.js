c76250faa38af3d3f00097e67dbdff8cbdd0f6ef
Agrarvolution <kilian.hartinger@aon.at>
2019-01-19 20:46:38 +0100

    Update and improve XMPprocessing
    Update to ECMAScript6
    Improve steps
    Remove additional step for checking if ticked

--- AutoTaggingInspector/js/xmpProcessor.js
+++ AutoTaggingInspector/js/xmpProcessor.js
@@ -16,98 +16,96 @@ function loadXMPContent () {
 }
 
 function processXMPContent(xmpContent) {
-    let response = [];
-
+    let responseHierarchy = [];
     //reverse child to parents relationship to parent to children
-    for (let i = 0; i < xmpContent.response.length; i++) {
+    for (let childIndex = 0; childIndex < xmpContent.response.length; childIndex++) {
         var parentIndices = [];
 
         //insert parents
-        for (let parentIndex = 0; parentIndex < xmpContent.response[i].parents.length; parentIndex++) {
-            let name = xmpContent.response[i].parents[parentIndex].description;
+        for (let parentIndex = 0; parentIndex < xmpContent.response[childIndex].parents.length; parentIndex++) {
+            let name = xmpContent.response[childIndex].parents[parentIndex].description;
 
             if (name === undefined) {
-                name = xmpContent.response[i].parents[parentIndex].name;
+                name = xmpContent.response[childIndex].parents[parentIndex].name;
             }
 
-            let index = findInHierarchy(response, name);
+            let index = findInHierarchy(responseHierarchy, name);
             let histIndex = findInHistory(xmpContent.history, name);
             //check if parent terminated
             if ((index < 0 && histIndex < 0) || (histIndex >= 0 && history[histIndex].property !== "terminate")) {
-                response.push({
+                responseHierarchy.push({
                     name: name,
                     confidence: 1.0,
                     children: [],
                     ticked: false
                 });
-                parentIndices.push(response.length - 1);
+                parentIndices.push(responseHierarchy.length - 1);
             } else {
                 parentIndices.push(index);
             }
         }
 
-        let histIndex = findInHistory(history, xmpContent.response[i].description);
+        let histIndex = findInHistory(xmpContent.history, xmpContent.response[childIndex].description);
         //terminate child
         if (histIndex < 0 || (histIndex >= 0 && xmpContent.history[histIndex].property !== "terminate")) {
             //setup child
             let child = {
-                name: tagList[i].description,
-                confidence: tagList[i].confidence,
+                name: xmpContent.response[childIndex].description,
+                confidence: xmpContent.response[childIndex].confidence,
                 children: [],
                 ticked: false
             };
             for (let parentIndex = 0; parentIndex < parentIndices.length; parentIndex++) {
-                if (parentIndices >= 0 && findInHierarchy(response[parentIndices[parentIndex]], tagList[i].description) < 0) {
+                if (parentIndices >= 0 && findInHierarchy(responseHierarchy[parentIndices[parentIndex]], xmpContent.response[childIndex].description) < 0) {
                     //set parent reference
-                    response[parentIndices[parentIndex]].children.push(child);
+                    responseHierarchy[parentIndices[parentIndex]].children.push(child);
                 }
             }
             if (parentIndices.length === 0) {
-                response.push(child);
+                responseHierarchy.push(child);
             }
         }
     }
 
     //get hierarchical object
-    var nodeHierarchy = [];
-    for (i = 0; i < hierarchy.length; i++) {
-        var writtenTags = [];
-        var indices = [];
+    let nodeHierarchy = [];
+    for (let i = 0; i < xmpContent.hierarchy.length; i++) {
+        let writtenTags = [];
+        let indices = [];
 
-        var hierarchyText = hierarchy[i].toString();
 
-        //delete empty strings (in case of wrong insertion into XMP -> |value| instead of value|value|value)
-        hierarchyText = hierarchyText.replace("/^\\s+|\\s+$/g", "").replace("[ ]{2,}", " ").replace("/^\||\|$/", "");
+        //delete empty strings (in case of wrong insertion into XMP -> |value| or |value||value instead of value|value|value)
+        xmpContent.hierarchy[i] = xmpContent.hierarchy[i].trim().replace("/^\||\|$/g", "").replace("/[\\|]{2,}/g", "|");
         //find division
-        for (var charPosition = 0; charPosition < hierarchyText.length; charPosition++) {
-            if (hierarchyText.charAt(charPosition) === "|") {
+        for (let charPosition = 0; charPosition < xmpContent.hierarchy[i].length; charPosition++) {
+            if (xmpContent.hierarchy[i].charAt(charPosition) === "|") {
                 indices.push(charPosition);
             }
         }
         //extract text
-        var currentIndex = 0, lastIndex = 0;
-        for (charPosition = 0; charPosition <= indices.length; charPosition++) {
+        let currentIndex = 0, lastIndex = 0;
+        for (let charPosition = 0; charPosition <= indices.length; charPosition++) {
             if (charPosition < indices.length) {
                 currentIndex = indices[charPosition];
             } else {
-                currentIndex = hierarchyText.length;
+                currentIndex = xmpContent.hierarchy[i].length;
             }
-            writtenTags.push(hierarchyText.substr(lastIndex, currentIndex - lastIndex));
+            writtenTags.push(xmpContent.hierarchy[i].substr(lastIndex, currentIndex - lastIndex));
 
             lastIndex = currentIndex + 1;
         }
 
         //insert into node tree
-        var tempHierarchy = nodeHierarchy;
-        for (var tagIndex = 0; tagIndex < writtenTags.length; tagIndex++) {
+        let tempHierarchy = nodeHierarchy;
+        for (let tagIndex = 0; tagIndex < writtenTags.length; tagIndex++) {
             //handle parent -> exclude from loop cause always there?
-            index = findInHierarchy(tempHierarchy, writtenTags[tagIndex]);
+            let index = findInHierarchy(tempHierarchy, writtenTags[tagIndex]);
             if (index < 0) {
                 tempHierarchy.push({
                     name: writtenTags[tagIndex],
                     confidence: 1.0,
                     children: [],
-                    ticked: false
+                    ticked: isTicked(writtenTags[tagIndex], xmpContent.subjects)
                 });
                 tempHierarchy = tempHierarchy[tempHierarchy.length - 1].children;
             } else {
@@ -115,48 +113,41 @@ function processXMPContent(xmpContent) {
             }
         }
     }
-    //check ticks
-    depthSearchTick(nodeHierarchy,subjects);
 
-    if(response !== undefined)
+    //combine written tags and reponse tags -> could be made into a depth/breadth traverse method
+    for (let childIndex = 0; i < responseHierarchy.length; i++)
     {
-        //combine written tags and reponse tags -> could be made into a depth/breadth traverse method
-        for (i = 0; i < response.length; i++)
+        let index = findInHierarchy(nodeHierarchy, responseHierarchy[i].name);
+        if (index >= 0)
         {
-            index = findInHierarchy(nodeHierarchy, response[i].name);
-            //$.writeln(index + " " + response[i].name);
-            if (index >= 0)
+            nodeHierarchy[index].confidence = responseHierarchy[i].confidence;
+            for (var ci = 0; ci < responseHierarchy[i].children; ci++)
             {
-                nodeHierarchy[index].confidence = response[i].confidence;
-                for (var ci = 0; ci < response[i].children; ci++)
+                var cIndex = findInHierarchy(nodeHierarchy[index].children, responseHierarchy[i].children[ci].name);
+                if (cIndex > 0)
                 {
-                    var cIndex = findInHierarchy(nodeHierarchy[index].children, response[i].children[ci].name);
-                    if (cIndex > 0)
-                    {
-                        nodeHierarchy[index].children[cIndex].confidence = response[i].children[ci].confidence;
-                    }
-                    else
-                    {
-                        //add if non existent
-                        nodeHierarchy[index].children.push(response[i].children[ci]);
-                    }
+                    nodeHierarchy[index].children[cIndex].confidence = responseHierarchy[i].children[ci].confidence;
+                }
+                else
+                {
+                    //add if non existent
+                    nodeHierarchy[index].children.push(responseHierarchy[i].children[ci]);
                 }
             }
-            else
-            {
-                //add if non existent
-
-                nodeHierarchy.push(response[i]);
-
-            }
+        }
+        else
+        {
+            //add if non existent
+            nodeHierarchy.push(responseHierarchy[i]);
         }
     }
 
-    //$.writeln("Combined tags & response");
+    //$.writeln("Combined tags & responseHierarchy");
     sortArrayOutput(nodeHierarchy);
 
-    let updateGUIEvent = new Event('updateGUI', {data: {response: xmpContent.response, content: nodeHierarchy, history: xmpContent.history}
-    });
+    let updateGUIEvent = new Event('updateGUI', {data: {
+        response: xmpContent.response, content: nodeHierarchy, history: xmpContent.history
+    }});
     document.dispatchEvent(updateGUIEvent);
 }
 
@@ -189,4 +180,55 @@ function findInHistory (history, nodeString)
         }
     }
     return -1;
+}
+
+/**
+ * traverse through object tree to sort all child nodes
+ * @param {array} outPutArray
+ */
+function sortArrayOutput (outPutArray)
+{
+    var traverseStack = [];
+    traverseStack.push(outPutArray);
+    while (traverseStack.length !== 0)
+    {
+        var array = traverseStack.pop();
+        sortOutput(array);
+        for (var i = 0; i < array.length; i++)
+        {
+            traverseStack.push(array[i].children);
+        }
+    }
+}
+
+/**
+ * Sort item array by descending confidence and by ascending name
+ * @param {array} outputObj
+ */
+function sortOutput (outputObj)
+{
+    outputObj.sort(function(a, b) {
+        return a.name.toLowerCase() === b.name.toLowerCase() ?
+            (a.confidence === b.confidence ? 0 : (a.confidence < b.confidence ? 1 : -1))
+            : (a.name.toLowerCase() > b.name.toLowerCase() ? 1 : -1);
+    });
+}
+
+
+/**
+ * Searches through searchArray if name exists
+ * @param {string} name
+ * @param {array} searchArray
+ * @return true if item exists (is ticked), false if it doesn't
+ */
+function isTicked(name, searchArray)
+{
+    for (var i = 0; i < searchArray.length; i++)
+    {
+        if (searchArray[i].toLowerCase() === name.toLowerCase())
+        {
+            return true;
+        }
+    }
+    return false;
 }
