f1d973b3b4887bcc4dcc85a03e4ef5c415fbb59a
Agrarvolution <kilian.hartinger@aon.at>
2019-01-22 18:28:19 +0100

    Removed placeholder call

--- AutoTaggingInspector/js/index.js
+++ AutoTaggingInspector/js/index.js
@@ -159,42 +159,6 @@ return parsed object
  */
 function loadContentListener(event)
 {
-    // placeholder -> replace by call to jsx & XMP parser
-    /*let answer = '{"response":[{"description":"dog","confidence":0.8858542056,"parents":[\n' +
-        '\n' +
-        ']},{"description":"vertebrate","confidence":0.73445880411272,"parents":[\n' +
-        '\n' +
-        ']},{"description":"clumber spaniel","confidence":0.72049342313895,"parents":[\n' +
-        '\n' +
-        ']},{"description":"mammal","confidence":0.718383380625,"parents":[\n' +
-        '\n' +
-        ']},{"description":"english cocker spaniel","confidence":0.57500941128906,"parents":[\n' +
-        '\n' +
-        ']},{"description":"Laubwald","confidence":0.9604,"parents":[{"name":"Landschaft"}]},{"description":"Brücke","confidence":0.81,"parents":[{"name":"Bauwerke"}]}],"content":[{"name":"Bauwerke","confidence":1,"children":[{"name":"Brücke","confidence":1,"children":[\n' +
-        '\n' +
-        '],"ticked":true},{"name":"Haus","confidence":1,"children":[\n' +
-        '\n' +
-        '],"ticked":true},{"name":"Ufermauer","confidence":1,"children":[\n' +
-        '\n' +
-        '],"ticked":true},{"name":"Ötzi","confidence":1,"children":[\n' +
-        '\n' +
-        '],"ticked":true}],"ticked":true},{"name":"Landschaft","confidence":1,"children":[{"name":"Laubwald","confidence":0.9604,"children":[\n' +
-        '\n' +
-        '],"ticked":false}],"ticked":false},{"name":"Landschaft","confidence":1,"children":[{"name":"Laubwald","confidence":1,"children":[\n' +
-        '\n' +
-        '],"ticked":true}],"ticked":false},{"name":"dog","confidence":0.8858542056,"children":[\n' +
-        '\n' +
-        '],"ticked":true},{"name":"vertebrate","confidence":0.73445880411272,"children":[\n' +
-        '\n' +
-        '],"ticked":false},{"name":"clumber spaniel","confidence":0.72049342313895,"children":[\n' +
-        '\n' +
-        '],"ticked":false},{"name":"mammal","confidence":0.718383380625,"children":[\n' +
-        '\n' +
-        '],"ticked":false},{"name":"english cocker spaniel","confidence":0.57500941128906,"children":[\n' +
-        '\n' +
-        '],"ticked":false}],"history":""}';
-    */
-
     //let content = JSON.parse(answer);
     //Enable for SendMetaDataHandler events
     if (event.data && event.data.type)
